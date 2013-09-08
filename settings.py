b54fa13620c4afca81770d1bdba522d47f45861c
Anton Novosyolov <anton.novosyolov@gmail.com>
2013-09-08 22:16:47 +0400

    Fix

--- settings.py
+++ settings.py
@@ -9,7 +9,7 @@ path = lambda root,*a: os.path.join(root, *a)
 ROOT = os.path.dirname(os.path.abspath(__file__))
 
 ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
+    ('Anton Novosyolov', 'anton.novosyolov@gmail.com')
 )
 
 MANAGERS = ADMINS
@@ -28,7 +28,7 @@ DATABASES = {
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
